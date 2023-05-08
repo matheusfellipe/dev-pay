b8d2ba40129b97d68b4c2e73e22082f7cfe9b940
Matheus <matheus060399@gmail.com>
2023-05-07 23:39:47 -0400

    feat: ensure authentication middleware

---
+++ src/infra/shared/http/middleware/ensure-authenticate.middleware.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { JWTToken } from "../../token/jwt.token";
+
+
+export const ensureAuthenticate = (request:Request,response:Response,next:NextFunction) => {
+    const headerAuth = request.headers.authorization;
+
+    if(!headerAuth){
+       return response.status(401).json({
+        error:'Token is missing',
+       })
+    }
+
+    const [, token] = headerAuth.split(' ')
+
+    if(!token){
+        return response.status(401).json({
+            error:'Token is missing',
+           })
+    }
+   
+    const verifyToken = new JWTToken().validate(token);
+
+    if(verifyToken){
+        request.userId = verifyToken.sub
+        return next()
+    }
+
+    return response.status(401).json({
+        error: 'Token invalid!',
+    })
+}
