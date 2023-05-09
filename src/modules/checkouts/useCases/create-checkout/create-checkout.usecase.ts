07b928252683f6788b086889b5c52af4c766a29b
Matheus Fellipe <matheus060399@gmail.com>
2023-05-09 11:53:30 -0400

    feat: handling checkout operation

---
+++ src/modules/checkouts/useCases/create-checkout/create-checkout.usecase.ts
@@ -0,0 +1,46 @@
+import { CustomError } from "../../../../errors/custom.error";
+import { User } from "../../../users/entities/user.entity";
+import { Seller } from "../../../sellers/entities/seller.entity";
+import { Checkout } from "../../entities/checkout.entity";
+import { IUserRepository } from "../../../users/repositories/user.repository";
+import { ICheckoutRepository } from "../../repositories/checkout.repository";
+import { ISellerRepository } from "../../../sellers/repositories/seller.repository";
+
+export type CreateCheckoutRequest = {
+    price: number
+    description: string
+    card_number: string
+    card_owner: string
+    card_expiring_date: Date
+    cvv: string
+    userId:string
+    sellerId:string
+    createdAt:Date
+}
+
+export class CreateCheckoutUseCase {
+    constructor(
+        private useRepository:IUserRepository,
+        private sellerRepository:ISellerRepository,
+        private checkoutRepository: ICheckoutRepository
+    ){}
+
+    async execute(data:CreateCheckoutRequest){
+        const checkout = new Checkout(data);
+
+        const user = await this.useRepository.findById(data.userId)
+
+        const seller = await this.sellerRepository.findById(data.sellerId);
+
+        if(!user){
+            throw new CustomError('User doens not exists!',400);
+        }
+
+        if(!seller){
+            throw new CustomError('Seller doens not exists!',400);
+        }
+
+        const checkoutCreated = await this.checkoutRepository.save(checkout);
+        return checkoutCreated
+    }
+}
---
+++ src/utils/date.ts
@@ -0,0 +1,38 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+
+export function formatDateHour(time: string) {
+    const date = dayjs().format('YYYY-MM-DD ') //2022-12-25
+    const dateTimeFormat = new Date(`${date} ${time}`) // 2022-12-25 23:55
+    return dayjs(dateTimeFormat)
+  }
+
+export function getDayOfWeek(date: string) {
+    return dayjs(date).day()
+  }
+  
+  export function formatDate(date: Date, format: string) {
+    return dayjs(date).format(format)
+  }
+  
+  export function formatDateUTC(date: Date, format: string) {
+    return dayjs(date).utc().format(format)
+  }
+  
+  export function dateToString(date: Date) {
+    return dayjs(date).format('YYYY-MM-DD').toString()
+  }
+  
+  export function toDate(date: Date) {
+    return dayjs(date).toDate()
+  }
+  
+  export function startOfDay() {
+    return dayjs().utc().startOf('D').toDate()
+  }
+  
+  export function endOfDay() {
+    return dayjs().utc().endOf('D').toDate()
+  }
