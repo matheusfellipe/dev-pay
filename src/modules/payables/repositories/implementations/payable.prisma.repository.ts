fef1be50c12c1defe6a6477b16dc888591144f1c
Matheus Fellipe <matheus060399@gmail.com>
2023-05-09 17:20:01 -0400

    feat:payables useCases

---
+++ src/modules/payables/repositories/implementations/payable.prisma.repository.ts
@@ -0,0 +1,19 @@
+import { prismaClient } from "../../../../infra/database/prisma.config";
+
+import { PayableWithSellerDTO } from "../../dto/payable.dto";
+
+import { Payables } from "../../entities/payable.entity";
+
+import { PayableMapper } from "../../mapper/payable.map";
+
+import { IPayableRepository } from "../payable.repository";
+
+export class PayablePrismaRepository implements IPayableRepository{
+    async save(data: Payables): Promise<Payables> {
+       const payable = await prismaClient.payable.create({
+        data:PayableMapper.entityToPrisma(data),
+       })
+       return PayableMapper.prismaToEntity(payable)
+    }
+
+}
